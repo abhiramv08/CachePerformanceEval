9f7029586948a00463da45a3eee10a62ca625234
Abhiram V <abhiramv08@gmail.com>
2023-09-25 01:06:16 -0400

    Update random_matrix_generator to generate only 1 matrix

--- utils/random_matrix_generator.py
+++ utils/random_matrix_generator.py
@@ -8,7 +8,7 @@ from scipy import sparse
 
 parser = argparse.ArgumentParser(description='Input Matrix generator')
 parser.add_argument('--seed', type=int, default=0, help='Seed Value')
-parser.add_argument('--n', type=int, default=3, help='Matrix order(nxn)')
+parser.add_argument('--n', type=int, default=5, help='Matrix order(nxn)')
 parser.add_argument('--sparsity', type=int, default=0, help='Sparisty of matrix in %')
 parser.add_argument('--dump', type=str, default='input_matrix.in', help='File name')
 
@@ -21,18 +21,16 @@ def createRandomMatrix(n):
     return matrix
 
 
-def saveMatrix(matrixA, matrixB, filename):
+def saveMatrix(matrixA, filename):
     if os.path.exists(filename): 
         os.remove(filename)
     else:   
         print("New file created: ",filename)
     f = open(filename, "w")
-    for i, matrix in enumerate([matrixA, matrixB]):
-        if i != 0:
-            f.write("\n")
-        for line in matrix:
-            #pdb.set_trace()
-            f.write("\t".join(map(str, line)) + "\n")
+
+    for line in matrixA:
+        #pdb.set_trace()
+        f.write("\t".join(map(str, line)) + "\n")
 
 
 def saveCSRMatrix(CSR_matrix, filename):
@@ -61,7 +59,6 @@ def main():
     outpath = args.dump
     #Create dense matrix
     matrixA = createRandomMatrix(n)
-    matrixB = createRandomMatrix(n)
     #print(matrixA)
     #Convert to sparse matrix by replacing value below threshold to 0
     if (args.sparsity):
@@ -71,27 +68,16 @@ def main():
                            size=int(matrixA.size * (args.sparsity/100)))
         flatA  = matrixA.flatten()
         flatA[indicesA] = 0
-        #Replace random x %element to 0 in matrixB
-        matrixB = np.asarray(matrixB)
-        indicesB = np.random.choice(np.arange(matrixB.size), replace=False,
-                           size=int(matrixB.size * (args.sparsity/100)))
-        flatB  = matrixB.flatten()
-        flatB[indicesB] = 0
         #Reshape it back to square matrix    
         flatA = flatA.reshape(n,n)
-        flatB = flatB.reshape(n,n)
         #print(flatA)
         matrixA_csr = sparse.csr_matrix(flatA)
         #print(matrixA_csr)
-        matrixB_csr = sparse.csr_matrix(flatB)
         matrixA = flatA.tolist()
-        matrixB = flatB.tolist()
         csr_Amatrix = "csrA_"+args.dump
-        csr_Bmatrix = "csrB_"+args.dump
         saveCSRMatrix(matrixA_csr, csr_Amatrix)
-        saveCSRMatrix(matrixB_csr, csr_Bmatrix)
         #print(matrixA)
-    saveMatrix(matrixA, matrixB, args.dump)
+    saveMatrix(matrixA, args.dump)
 
 if __name__ == '__main__':
     main()
